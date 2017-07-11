5897d76b57b844020e4ce5da51731d05f59b8ff2
Stefan Radenkovic <sradenkovic@hstechnology.net>
2017-07-11 10:28:13 +0200

    routes and configuration changed

--- Configuration.php
+++ Configuration.php
@@ -1,4 +1,12 @@
 <?php
     abstract class Configuration {
+        const DB_HOST = 'localhost';
+        const DB_NAME = 'katalog_200688';
+        const DB_USER = 'root';
+        const DB_PASS = '';
+
+        const BASE_PATH = '/MVC/';
+        const BASE_URL = 'http://localhost/MVC/';
         
+        const USER_SALT = '0896086hgodfhgouhy98ugodfgodfhgu54hy98yugobrglrthg98uyt9rhgu56uruyrufgjyt65uujfgjf56';
     }
--- Routes.php
+++ Routes.php
@@ -1,6 +1,83 @@
 <?php
     return [
         [
+            'Pattern'    => '|^product/([0-9]+)/?$|',
+            'Controller' => 'Product',
+            'Method'     => 'view'
+        ],
+        [
+            'Pattern'    => '|^category/([0-9]+)/?$|',
+            'Controller' => 'Category',
+            'Method'     => 'view'
+        ],
+        [
+            'Pattern'    => '|^login/?$|',
+            'Controller' => 'Main',
+            'Method'     => 'login'
+        ],
+        [
+            'Pattern'    => '|^logout/?$|',
+            'Controller' => 'Main',
+            'Method'     => 'logout'
+        ],
+        [
+            'Pattern'    => '|^kontakt/?$|',
+            'Controller' => 'Main',
+            'Method'     => 'kontakt'
+        ],
+
+        # Rute za administratore
+        [
+            'Pattern'    => '|^admin/categories/store/}?$|',
+            'Controller' => 'AdminCategory',
+            'Method'     => 'store'
+        ],
+        [
+            'Pattern'    => '|^admin/categories/edit/[0-9]+}?$|',
+            'Controller' => 'AdminCategory',
+            'Method'     => 'edit'
+        ],
+        [
+            'Pattern'    => '|^admin/categories/delete/[0-9]+}?$|',
+            'Controller' => 'AdminCategory',
+            'Method'     => 'delete'
+        ],
+        [
+            'Pattern'    => '|^admin/devices/store/?$|',
+            'Controller' => 'AdminProducts',
+            'Method'     => 'store'
+        ],  
+        [
+            'Pattern'    => '|^admin/devices/edit/[0-9]+}?$|',
+            'Controller' => 'AdminProducts',
+            'Method'     => 'edit'
+        ],
+        [
+            'Pattern'    => '|^admin/devices/delete/[0-9]+}?$|',
+            'Controller' => 'AdminProducts',
+            'Method'     => 'delete'
+        ],
+        [
+            'Pattern'    => '|^admin/specifications/store/?$|',
+            'Controller' => 'AdminSpecifications',
+            'Method'     => 'store'
+        ],
+        [
+            'Pattern'    => '|^admin/specifications/edit/[0-9]+}?$|',
+            'Controller' => 'AdminSpecifications',
+            'Method'     => 'edit'
+        ],
+        [
+            'Pattern'    => '|^admin/specifications/delete/[0-9]+}?$|',
+            'Controller' => 'AdminSpecifications',
+            'Method'     => 'delete'
+        ],
+        [
+            'Pattern'    => '|^admin/$|',
+            'Controller' => 'AdminAll',
+            'Method'     => 'index'
+        ],
+        [
             'Pattern'    => '|^.*$|',
             'Controller' => 'Main',
             'Method'     => 'index'
